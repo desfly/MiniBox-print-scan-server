c4ec0fce9ad4fca79bd1ea6fb49fc0b6e9b48639
desfly <155248075+desfly@users.noreply.github.com>
2026-09-22 16:24:38 +0300

    Expose raw IPP format validation

--- src/minibox-ipp/ipp.h
+++ src/minibox-ipp/ipp.h
@@ -5,6 +5,7 @@
 enum { IPP_OP_PRINT_JOB=0x0002, IPP_OP_VALIDATE_JOB=0x0004, IPP_OP_GET_PRINTER_ATTRIBUTES=0x000b };
 struct ipp_request { uint8_t major,minor; uint16_t operation; uint32_t request_id; };
 int ipp_parse_header(const unsigned char *buf,size_t len,struct ipp_request *r);
+int ipp_raw_format_supported(const unsigned char *buf,size_t len);
 int ipp_document_offset(const unsigned char *buf,size_t len,size_t *offset);
 const char *ipp_operation_name(uint16_t op);
 size_t ipp_build_status(unsigned char *out,size_t cap,const struct ipp_request *r,uint16_t status);
