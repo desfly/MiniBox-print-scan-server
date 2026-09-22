f83f26ed7cd4d77f59431eb147d7b27c79b5ef8f
desfly <155248075+desfly@users.noreply.github.com>
2026-09-22 06:56:23 +0300

    feat: expose DNS-SD responder loop

--- src/minibox-discoveryd/mdns.h
+++ src/minibox-discoveryd/mdns.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <stddef.h>
+#include <signal.h>
 #include "service.h"
 
 int mb_mdns_publish_once(const mb_service_t *services, size_t count, const char *hostname);
+
+int mb_mdns_serve(const mb_service_t *services, size_t count, const char *hostname,
+                  volatile sig_atomic_t *stop);
