830ab8b322769e4f5e4c156badd334944d179620
desfly <155248075+desfly@users.noreply.github.com>
2026-09-24 21:41:50 +0300

    test(web): correct regex escape in packaging dependency check

--- tests/test-web-ui.sh
+++ tests/test-web-ui.sh
@@ -11,7 +11,7 @@ grep -q '/cgi-bin/minibox-status' "$root/minibox/app.js"
 grep -q 'RAW/JetDirect (порт 9100): не реалізовано' "$root/index.html"
 grep -q 'Фізичне сканування потребує окремого тесту' "$root/index.html"
 grep -Eq '^PKG_RELEASE:=13$' package/minibox-mfp/Makefile
-grep -Eq 'DEPENDS:=.*\\+uhttpd' package/minibox-mfp/Makefile
+grep -Eq 'DEPENDS:=.*\+uhttpd' package/minibox-mfp/Makefile
 grep -q '$(1)/www/index.html' package/minibox-mfp/Makefile
 grep -q '$(1)/www/cgi-bin/minibox-status' package/minibox-mfp/Makefile
 
