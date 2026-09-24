022fb364a54e660afead8d0f145352beaa574254
desfly <155248075+desfly@users.noreply.github.com>
2026-09-24 21:23:05 +0300

    ci(scan): exercise bulk-transfer failures in strict libusb compile check

--- tests/compile-libusb-backend.sh
+++ tests/compile-libusb-backend.sh
@@ -7,6 +7,8 @@ cc $CFLAGS -c src/minibox-usb/libusb_m1522.c -o /tmp/libusb_m1522.o
 cc $CFLAGS tests/test-printer-interface.c /tmp/libusb_m1522.o $LIBS -o /tmp/test-printer-interface
 /tmp/test-printer-interface
 cc $CFLAGS -c src/minibox-usb/scan_m1522.c -o /tmp/scan_m1522.o
+cc $CFLAGS tests/test-scan-usb-errors.c $LIBS -o /tmp/test-scan-usb-errors
+/tmp/test-scan-usb-errors
 cc $CFLAGS -c src/minibox-scan/soapht_transport.c -o /tmp/soapht_transport.o
 cc $CFLAGS -c src/minibox-scan/soapht_m1522_io.c -o /tmp/soapht_m1522_io.o
 cc $CFLAGS -c src/minibox-scan/soapht_codec.c -o /tmp/soapht_codec.o
