5a45fbd6f73d167eed085f5b8389d66d5d510b3c
desfly <155248075+desfly@users.noreply.github.com>
2026-09-25 06:40:57 +0300

    tests: require WSD daemon procd supervision

--- tests/test-procd-runtime.sh
+++ tests/test-procd-runtime.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -eu
-for f in overlay/etc/init.d/minibox-printerd overlay/etc/init.d/minibox-scand overlay/etc/init.d/minibox-discoveryd; do
+for f in overlay/etc/init.d/minibox-printerd overlay/etc/init.d/minibox-scand overlay/etc/init.d/minibox-discoveryd overlay/etc/init.d/minibox-wsdd; do
   grep -q 'USE_PROCD=1' "$f"
   grep -q 'procd_set_param command' "$f"
 done
 grep -q ' 631' overlay/etc/init.d/minibox-printerd
 grep -q ' 8080' overlay/etc/init.d/minibox-scand
+grep -q '/usr/sbin/minibox-wsdd' overlay/etc/init.d/minibox-wsdd
 echo 'procd runtime contract OK'
